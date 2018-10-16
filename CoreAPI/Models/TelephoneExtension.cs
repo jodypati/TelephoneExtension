203398ee54a05a45598da190cf439026c9800118
Sausa Jodypati <jodypati@gmail.com>
2018-10-16 13:31:46 +0700

    adding model for telephone extension

---
+++ CoreAPI/Models/TelephoneExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CoreAPI.Models
+{
+    public class TelephoneExtension
+    {
+        public DateTime BeginDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int RecordId { get; set; }
+        public int ParentId { get; set; }
+        public string ExtensionType { get; set; }
+        public string ExtensionName{ get; set; }
+        public string Number { get; set; }
+        public DateTime ChangeDate { get; set; }
+        public string UserChanger { get; set; }
+    }
+}
