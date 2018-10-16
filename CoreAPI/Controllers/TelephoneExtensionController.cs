2b64825989c38cf9daa021dbb1ee4c4efc3c05e2
Sausa Jodypati <jodypati@gmail.com>
2018-10-16 13:27:52 +0700

    create controller for routing get All and specific using record ID

--- CoreAPI/Controllers/TelephoneExtensionController.cs
+++ CoreAPI/Controllers/TelephoneExtensionController.cs
@@ -3,79 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using CoreAPI.DataProvider;
 using CoreAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace CoreAPI.Controllers
 {
-    [Route("api/v1/[controller]")]
-    public class TelephoneExtensionController : ControllerBase
+    [Route("api/[controller]")]
+    public class TelephoneextensionController : Controller
     {
-        private readonly TelephoneExtensionContext _telephoneExtensionContext;
-        //private readonly ICatalogIntegrationEventService
-        public TelephoneExtensionController(TelephoneExtensionContext context)
+        private ITelephoneExtensionDataProvider extensionDataProvider;
+
+        public TelephoneextensionController(ITelephoneExtensionDataProvider e)
         {
-            _telephoneExtensionContext = context ?? throw new ArgumentNullException(nameof(context));
-            ((DbContext)context).ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            this.extensionDataProvider = e;
         }
 
-        // GET api/v1/[controller]/extensions
         [HttpGet]
-        [Route("extensions")]
-        [ProducesResponseType(typeof(List<TelephoneExtensionItem>), (int)HttpStatusCode.OK)]
-        public async Task<IActionResult> TelephoneExtensions()
+        public async Task<IEnumerable<TelephoneExtension>> Get()
         {
-            var items = await _telephoneExtensionContext.TelephoneItems.ToListAsync();
-
-            return Ok(items);
+            return await this.extensionDataProvider.GetExtensions();
         }
 
-        //POST api/v1/[controller]/items
-        [Route("extensions")]
+        [HttpGet("{RecordId}")]
+        public async Task<TelephoneExtension> Get(int RecordId)
+        {
+            return await this.extensionDataProvider.GetExtension(RecordId);
+        }
+
         [HttpPost]
-        [ProducesResponseType((int)HttpStatusCode.Created)]
-        public async Task<IActionResult> CreateProduct([FromBody]TelephoneExtensionItem telephoneExtension)
+        public async Task Post([FromBody]TelephoneExtension telephoneExtension)
         {
-            var item = new TelephoneExtensionItem
-            {
-                BeginDate = telephoneExtension.BeginDate,
-                EndDate = telephoneExtension.EndDate,
-                RecordId = telephoneExtension.RecordId,
-                ParentId = telephoneExtension.ParentId,
-                Number = telephoneExtension.Number,
-                UserChanger = telephoneExtension.UserChanger
-            };
-            _telephoneExtensionContext.TelephoneItems.Add(item);
-
-            await _telephoneExtensionContext.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetItemById), new { id = item.RecordId }, null);
-        }
-
-        [HttpGet]
-        [Route("extensions/{id:int}")]
-        [ProducesResponseType((int)HttpStatusCode.NotFound)]
-        [ProducesResponseType(typeof(TelephoneExtensionItem), (int)HttpStatusCode.OK)]
-        public async Task<IActionResult> GetItemById(int id)
-        {
-            if (id <= 0)
-            {
-                return BadRequest();
-            }
-
-            var item = await _telephoneExtensionContext.TelephoneItems.SingleOrDefaultAsync(ci => ci.RecordId == id);
-
-            //var baseUri = _settings.PicBaseUrl;
-            //var azureStorageEnabled = _settings.AzureStorageEnabled;
-            //item.FillProductUrl(baseUri, azureStorageEnabled: azureStorageEnabled);
-
-            if (item != null)
-            {
-                return Ok(item);
-            }
-
-            return NotFound();
+            await this.extensionDataProvider.AddExtension(telephoneExtension);
         }
 
     }
