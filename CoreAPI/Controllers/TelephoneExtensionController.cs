1f8a26e2525bcd054b65b2841d5afffc755604b1
Sausa Jodypati <jodypati@gmail.com>
2018-11-26 06:37:13 +0700

    implement action on every method for extension controller

--- CoreAPI/Controllers/TelephoneExtensionController.cs
+++ CoreAPI/Controllers/TelephoneExtensionController.cs
@@ -5,15 +5,23 @@ using System.Net;
 using System.Threading.Tasks;
 using CoreAPI.DataProvider;
 using CoreAPI.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace CoreAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    [Route("api/v1/[controller]")]
     public class TelephoneextensionController : Controller
     {
         private ITelephoneExtensionDataProvider extensionDataProvider;
+
+        public string Protected()
+        {
+            return "Only if you have a valid token!";
+        }
 
         public TelephoneextensionController(ITelephoneExtensionDataProvider e)
         {
@@ -26,17 +34,46 @@ namespace CoreAPI.Controllers
             return await this.extensionDataProvider.GetExtensions();
         }
 
-        [HttpGet("{RecordId}")]
-        public async Task<TelephoneExtension> Get(int RecordId)
+        [HttpGet("{RecordId:int}")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(TelephoneExtension), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetRecordById(int RecordId)
         {
-            return await this.extensionDataProvider.GetExtension(RecordId);
+            if (RecordId <= 0)
+            {
+                return BadRequest();
+            }
+            var extension = await this.extensionDataProvider.GetExtension(RecordId);
+            if (extension != null)
+            {
+                return Ok(extension);
+            }
+            return NotFound();
         }
 
         [HttpPost]
-        public async Task Post([FromBody]TelephoneExtension telephoneExtension)
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        public async Task<IActionResult> Post([FromBody]TelephoneExtension telephoneExtension)
         {
-            await this.extensionDataProvider.AddExtension(telephoneExtension);
+            var id = await this.extensionDataProvider.AddExtension(telephoneExtension);
+            return CreatedAtAction(nameof(GetRecordById), new { RecordId = id }, null);
+        }
+        [HttpPut]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        public async Task<IActionResult> UpdateTelephoneExtension([FromBody]TelephoneExtension telephoneExtension)
+        {
+            await this.extensionDataProvider.UpdateExtension(telephoneExtension);
+            return CreatedAtAction(nameof(GetRecordById), new { RecordId = telephoneExtension.RecordId }, null);
         }
 
+        [Route("{id}")]
+        [HttpDelete]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            await this.extensionDataProvider.DeleteExtension(id);
+            return NoContent();
+        }
     }
 }
